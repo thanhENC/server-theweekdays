a31612fab25d1416fcc5bc0a1d840104e70fd6f3
paferuto <haodh20411@st.uel.edu.vn>
2023-04-19 16:29:03 +0700

    Revert ""collection api""
    This reverts commit ba65329d26f6df7261e530e447b5fedb88917ca8.

---
+++ routes/collection.js
@@ -0,0 +1,73 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { collection_collection } = require("../utils/mongo");
+const { ObjectId } = require("mongodb");
+const { Collection } = require("mongoose");
+
+// =================SETTING UP ROUTES=================
+// prefix: /v1/lookbook
+
+// 1. get all collection
+router.get("/", async (req, res) => {
+  try {
+    // get all products from database and send specific fields
+    let result = await collection_collection.find({}).toArray();
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// 2. get collection by id
+router.get("/:id", async (req, res) => {
+  try {
+    oid = new ObjectId(req.params.id);
+    const result = await collection_collection.findOne({ _id: oid });
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// 3. post new collection
+router.post("/", async (req, res) => {
+  try {
+    const result = await collection_collection.insertOne(req.body);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// 4. update collection by id
+router.put("/", async (req, res) => {
+  //update json Fashion into database
+  await collection_collection.updateOne(
+    { _id: new ObjectId(req.body._id) }, //condition for update
+    {
+      $set: {
+        //Field for updating
+        name: req.body.name,
+        description: req.body.description,
+        lookbook: req.body.lookbook,
+      },
+    }
+  );
+  var o_id = new ObjectId(req.body._id);
+  const result = await collection_collection.find({ _id: o_id }).toArray();
+  res.send(result[0]);
+});
+
+//5. delete collection by id
+router.delete("/:id", async (req, res) => {
+  var o_id = new ObjectId(req.params["id"]);
+  const result = await collection_collection.find({ _id: o_id }).toArray();
+  await collection_collection.deleteOne({ _id: o_id });
+  res.send(result[0]);
+});
+
+// ===================================================
+
+module.exports = router;
